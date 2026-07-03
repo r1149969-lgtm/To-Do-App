06306f0973442e6e0ace6117996e54e9a791be64
Kavya R <r1149969@gmail.com>
2026-07-03 22:32:36 +0530

    Refactored To-Do App using functions

--- to-do.py
+++ to-do.py
@@ -1,5 +1,34 @@
 tasks=[]
 
+def add_task():
+    task = input("Enter a task:")
+    tasks.append(task)
+    print("Task added successfully!")
+    
+def view_tasks():
+    if len(tasks) == 0:
+        print("No tasks available.")
+    else:
+        print("\nYour Tasks:")
+        for i in range(len(tasks)):
+            print(f"{i + 1}. {tasks[i]}")
+
+def delete_task():
+    if len(tasks) == 0:
+        print("No task available.")
+    else:
+        print("\nYour Tasks:")
+        for i in range(len(tasks)):
+            print(f"{i+1}.{tasks[i]}")
+    delete = int(input("Enter task number to delete:"))
+    if delete >= 1 and delete <= len(tasks):
+        removed = tasks.pop(delete - 1)
+        print(f"{removed} deleted successfully!")
+    else:
+        print("Invalid task number.") 
+
+
+
 
 while True:
 
@@ -11,31 +40,11 @@ while True:
 
     choice = input("Enter your choise: ")
     if choice == "1":
-       task = input("Enter a task:")
-       tasks.append(task)
-       print("Task added successfully!")
-
+       add_task()
     elif choice == "2":
-       if len(tasks) == 0:
-          print("No tasks available.")
-       else:
-           print("\nYour Tasks:")
-           for i in range(len(tasks)):
-               print(f"{i + 1}. {tasks[i]}")
-
+        view_tasks()
     elif choice == "3":
-        if len(tasks) == 0:
-            print("No task available.")
-        else:
-            print("\nYour Tasks:")
-            for i in range(len(tasks)):
-                print(f"{i+1}.{tasks[i]}")
-            delete = int(input("Enter task number to delete:"))
-            if delete >= 1 and delete <= len(tasks):
-                removed = tasks.pop(delete - 1)
-                print(f"{removed} deleted successfully!")
-            else:
-                print("Invalid task number.") 
+        delete_task() 
     elif choice == "4":
        print("Thank you! Goodbye.")
        break
