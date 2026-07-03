3ffa13c45cefe2cc11729228cf8df3409c355505
Kavya R <r1149969@gmail.com>
2026-07-03 20:26:45 +0530

    Updated To-Do App with delete task feature

--- to-do.py
+++ to-do.py
@@ -1,5 +1,6 @@
 tasks=[]
 
+
 while True:
 
     print("\n===== To-Do App =====")
@@ -8,23 +9,36 @@ while True:
     print("3.Delete Task")
     print("4.Exit")
 
-    choise = input("Enter your choise: ")
-    if choise == "1":
+    choice = input("Enter your choise: ")
+    if choice == "1":
        task = input("Enter a task:")
        tasks.append(task)
        print("Task added successfully!")
 
-    elif choise == "2":
+    elif choice == "2":
        if len(tasks) == 0:
           print("No tasks available.")
-    else:
-        print("\nYour Tasks:")
-        for i in range(len(tasks)):
-            print("{i + 1}.{tasks[i]}")
-    elif choice == "3": 
-    print("Delete option will be added next.")
-    elif choise == "4":
-    print("Thank you! Goodbye.")
-    break
+       else:
+           print("\nYour Tasks:")
+           for i in range(len(tasks)):
+               print(f"{i + 1}. {tasks[i]}")
+
+    elif choice == "3":
+        if len(tasks) == 0:
+            print("No task available.")
+        else:
+            print("\nYour Tasks:")
+            for i in range(len(tasks)):
+                print(f"{i+1}.{tasks[i]}")
+            delete = int(input("Enter task number to delete:"))
+            if delete >= 1 and delete <= len(tasks):
+                removed = tasks.pop(delete - 1)
+                print(f"{removed} deleted successfully!")
+            else:
+                print("Invalid task number.") 
+    elif choice == "4":
+       print("Thank you! Goodbye.")
+       break
 else:
-    print("Invalid choice.Try again.")    
+       
+       print("Invalid choice.Try again.")   
